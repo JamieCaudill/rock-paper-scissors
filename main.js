c477cc89fe90957313e54bab58c944217a1e43b2
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-19 10:33:14 -0700

    feat: Add global variables to track wins

--- main.js
+++ main.js
@@ -9,19 +9,28 @@
 //   }
 // }
 
+var computerWins = 0;
+var playerWins = 0;
+
 function createGame(playerSelection, computerSelection) {
   if (playerSelection === 'rock' && computerSelection === 'paper') {
     console.log('Computer wins! Computer: ' + computerSelection)
+    return computerWins = computerWins++;
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-     console.log('Player wins! Computer: ' + computerSelection)
+    console.log('Player wins! Computer: ' + computerSelection)
+    return playerWins = playerWins++;
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     console.log('Computer wins! Computer: ' + computerSelection)
+    return computerWins = computerWins++;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     console.log('Player wins! Computer: ' + computerSelection)
+    return playerWins = playerWins++;
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     console.log('Computer wins! Computer: ' + computerSelection)
+    return computerWins = computerWins++;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     console.log('Player wins! Computer: ' + computerSelection)
+    return playerWins = playerWins++;
   } else {
     console.log('Draw!')
   }
